e58c4dd6ca70c8b311f94e51f53c9fa39f2c9728
Gavin Coombes <gjcoombes@gmail.com>
2014-01-09 19:28:31 +0800

    begin runcards dev

---
+++ jack/runcards.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Module: runcards.py
+Created on Thu Jan 09 19:10:40 2014
+@author: gav
+Description:
+Generate and verify runcards for winfates
+"""
+### Imports
+from __future__ import print_function
+
+### Logging
+import logging
+logging.basicConfig(level=logging.DEBUG)
+debug, info, error = logging.debug, logging.info, logging.error
+### Constants
+
+### Classes
+
+### Functions
+
+### Tests
+
+if __name__ == "__main__":
+
+    print("Done __main__")
